1027428519a6e4a8939af48fa97f5e7bf922ab10
akash03nigam <40694250+akash03nigam@users.noreply.github.com>
2021-06-02 09:28:13 +0530

    Update custom.py

--- testapp/templatetags/custom.py
+++ testapp/templatetags/custom.py
@@ -4,3 +4,7 @@ register= template.Library()
 @register.filter(name='trun') #This is a decorater
 def trun(value,n):
     return value[0:n]
+#def trun5(value):    #Another way to define custom filters
+    #name=value[0:5]
+    #return name
+    #register.filter('alias_name',trun5)
