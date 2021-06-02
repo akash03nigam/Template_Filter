25d20a5153b6cf0b1cd93ff1eb063e1f82494449
akash03nigam <40694250+akash03nigam@users.noreply.github.com>
2021-06-02 09:25:01 +0530

    Update custom.py

--- testapp/templatetags/custom.py
+++ testapp/templatetags/custom.py
@@ -1,6 +1,6 @@
 from django import template
 register= template.Library()
 
-@register.filter(name='trun')
+@register.filter(name='trun') #This is a decorater
 def trun(value,n):
     return value[0:n]
